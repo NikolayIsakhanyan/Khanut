a4a7517e87c0cc7afd223cf5277bf41a639a5447
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2023-01-02 16:06:04 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double sum ;
+      //  Console.Write( "nermutsel tiv= ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        string gorts;
+        gorts = Console.ReadLine();
+       // Console.Write( "nermutsel tiv= ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        switch (gorts)
+        {
+            case "+":
+                sum = x + y;
+                Console.WriteLine(sum);
+                break;
+            case "-":
+                sum = x - y;
+                Console.WriteLine(sum);
+                break;
+            case "*":
+                sum = x * y;
+                Console.WriteLine(sum);
+                break;
+            case "/":
+                try
+                {
+                sum = x / y;
+                    Console.WriteLine(sum);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine(e.Message);
+                    
+                }
+                finally
+                {Console.WriteLine("nermucel 0 ic baci urish tiv");   
+                }
+            
+                break;
+            default:
+                Console.WriteLine("katarel 4 gortsoghutyun");
+                break;
+        }
+
+
+
+    }
+
+
+}
---
+++ Khanut/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter Ashxatavardz -");
+       int ashVardz=Convert.ToInt32(Console.ReadLine());
+        double ekHark = (ashVardz * 21) / 100;
+        Console.WriteLine($"Ekamtayin harky kkazmi`{ekHark} amd,");
+        double socVchar = (ashVardz * 4.5) / 100;
+        Console.WriteLine($"Socialakan vchary`{socVchar} amd,");
+        double droshmVchar = (ashVardz * 1.5) / 100;
+        Console.WriteLine($"Zincarayoghneri Apahovagrutyan Himnadrami droshmanshani vchary `{droshmVchar} amd ,");
+        double maqur = ashVardz - ekHark - socVchar - droshmVchar;
+        Console.WriteLine($"Maqur mnacord `{maqur} amd:");
+
+    }
+}
